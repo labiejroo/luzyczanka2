339662885eddf7e050369c4c92559c09dae2d20f
Paweł Łabuz <pawel.labuz10@gmail.com>
2019-05-03 17:34:18 +0200

    Updates

---
+++ precache-manifest.ea4d30a8e2bc56660543d092531aa5af.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a251274a53dfa845a0eb0599b408807",
+    "url": "/mybuild/static/media/floor.8a251274.jpg"
+  },
+  {
+    "revision": "7e6f3db256098ce86d59",
+    "url": "/mybuild/static/css/main.edcaf55c.chunk.css"
+  },
+  {
+    "revision": "00241d73fa307dfc3105",
+    "url": "/mybuild/static/js/1.00241d73.chunk.js"
+  },
+  {
+    "revision": "2ed763f8762a3dfb595a",
+    "url": "/mybuild/static/js/runtime~main.2ed763f8.js"
+  },
+  {
+    "revision": "96cce1d18dcdbb9dfc66d73696f74465",
+    "url": "/mybuild/static/media/exterior.96cce1d1.jpg"
+  },
+  {
+    "revision": "b96ac11fb28877b3fa64573ffabbcc8c",
+    "url": "/mybuild/static/media/cake.b96ac11f.jpg"
+  },
+  {
+    "revision": "feefa930261264a689d294c2e6eded3b",
+    "url": "/mybuild/static/media/decoration.feefa930.jpg"
+  },
+  {
+    "revision": "d5f0d7c058a074d06396e858af5e798f",
+    "url": "/mybuild/static/media/exterior2.d5f0d7c0.jpg"
+  },
+  {
+    "revision": "7e6f3db256098ce86d59",
+    "url": "/mybuild/static/js/main.7e6f3db2.chunk.js"
+  },
+  {
+    "revision": "7023f1d39588b4a1c7fc4bf4e27b102c",
+    "url": "/mybuild/static/media/foodTable.7023f1d3.jpg"
+  },
+  {
+    "revision": "5cfe283d930dece5c434d0711b63c9c1",
+    "url": "/mybuild/static/media/foodTable2.5cfe283d.jpg"
+  },
+  {
+    "revision": "db015416a5535fa5da3856c7f949eb86",
+    "url": "/mybuild/static/media/interiorEmpty.db015416.jpg"
+  },
+  {
+    "revision": "5aea53e063e90e62a069bb6b1bdc5099",
+    "url": "/mybuild/static/media/interiorMainTable2.5aea53e0.jpg"
+  },
+  {
+    "revision": "e8e67cd74ef23865aa64ea318934886e",
+    "url": "/mybuild/static/media/mainTable2.e8e67cd7.jpg"
+  },
+  {
+    "revision": "fc53ba0e9c9bb6397baa3c4a8d3423cd",
+    "url": "/mybuild/static/media/sushi.fc53ba0e.jpg"
+  },
+  {
+    "revision": "9505eacb20eb486b4b1ee0e002a83e97",
+    "url": "/mybuild/static/media/interiorMainTable.9505eacb.jpg"
+  },
+  {
+    "revision": "dd20b14dd102de86b886e7e83eea4612",
+    "url": "/mybuild/index.html"
+  }
+];
